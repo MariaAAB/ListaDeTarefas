a4cdbc1c5d29c70f4cad63d75b0ae1975e3ee08a
MAria AAB <018996373635alves@gmail.com>
2025-07-21 14:54:05 -0300

    alterando os dias da semana que estava em miniculos para maiusculos

--- src/Components/Select.tsx
+++ src/Components/Select.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 function Select(props:any){
   return (
     <div>
        <select value={props.value} onChange={e=>props.setDiaSemana(e.target.value)} >
-        <option value=''>Escolha um dia da semana</option>
-        <option value='Domingo'>Domingo</option>
-        <option value='Segunda'>Segunda-feira</option>
-        <option value='terca'>Terca-feira</option>
-        <option value='Quarta'>quarta-feira</option>
-        <option value='Quinta'>quinta-feira</option>
-        <option value='Sexta'>Sexta-feira</option>
-        <option value='Sabado'>Sabado</option>
+        <option value=''>Escolha um dia da semana!</option>;
+        <option value='Domingo'>Domingo</option>;
+        <option value='Segunda'>Segunda-feira</option>;
+        <option value='Terca'>Terca-feira</option>;
+        <option value='Quarta'>Quarta-feira</option>;
+        <option value='Quinta'>Quinta-feira</option>;
+        <option value='Sexta'>Sexta-feira</option>;
+        <option value='Sabado'>Sabado</option>;
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default Select
+export default Select;
