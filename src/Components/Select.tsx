7c4db513dac3818a26b3e47375d41dac7237d5ba
MariaAAB <018996373635alves@gmail.com>
2025-07-23 16:31:25 -0300

    Revert "dando mais opcoes"
    This reverts commit e144f4efc5627464a9249a8e437cafadfe796560.

--- src/Components/Select.tsx
+++ src/Components/Select.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Select(props:any){
   return (
     <div>
-      <p>Escolha um dia da semana</p>
        <select value={props.value} onChange={e=>props.setDiaSemana(e.target.value)} >
         <option value=''>Escolha um dia da semana!</option>;
         <option value='Domingo'>Domingo</option>;
@@ -14,7 +13,6 @@ function Select(props:any){
         <option value='Sexta'>Sexta-feira</option>;
         <option value='Sabado'>Sabado</option>;
       </select>
-
     </div>
   );
 };
